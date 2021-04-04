7e9843c55274a2219c152de5b22ed6d5084752e5
docker liu <dockerliu@hotmail.com>
2021-04-04 15:43:50 +0100

    添加项目文件。

---
+++ RRQMXML/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace RRQMXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testGenerateXmlBtn = new System.Windows.Forms.Button();
+            this.testReadXmlBtn = new System.Windows.Forms.Button();
+            this.txtXML = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // testGenerateXmlBtn
+            // 
+            this.testGenerateXmlBtn.Location = new System.Drawing.Point(28, 21);
+            this.testGenerateXmlBtn.Name = "testGenerateXmlBtn";
+            this.testGenerateXmlBtn.Size = new System.Drawing.Size(95, 23);
+            this.testGenerateXmlBtn.TabIndex = 0;
+            this.testGenerateXmlBtn.Text = "测试生成XML";
+            this.testGenerateXmlBtn.UseVisualStyleBackColor = true;
+            this.testGenerateXmlBtn.Click += new System.EventHandler(this.testGenerateXmlBtn_Click);
+            // 
+            // testReadXmlBtn
+            // 
+            this.testReadXmlBtn.Location = new System.Drawing.Point(169, 21);
+            this.testReadXmlBtn.Name = "testReadXmlBtn";
+            this.testReadXmlBtn.Size = new System.Drawing.Size(95, 23);
+            this.testReadXmlBtn.TabIndex = 1;
+            this.testReadXmlBtn.Text = "测试读取XML";
+            this.testReadXmlBtn.UseVisualStyleBackColor = true;
+            this.testReadXmlBtn.Click += new System.EventHandler(this.testReadXmlBtn_Click);
+            // 
+            // txtXML
+            // 
+            this.txtXML.Location = new System.Drawing.Point(28, 50);
+            this.txtXML.Multiline = true;
+            this.txtXML.Name = "txtXML";
+            this.txtXML.Size = new System.Drawing.Size(258, 212);
+            this.txtXML.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 274);
+            this.Controls.Add(this.txtXML);
+            this.Controls.Add(this.testReadXmlBtn);
+            this.Controls.Add(this.testGenerateXmlBtn);
+            this.Name = "Form1";
+            this.Text = "RRQMXML实例";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testGenerateXmlBtn;
+        private System.Windows.Forms.Button testReadXmlBtn;
+        private System.Windows.Forms.TextBox txtXML;
+    }
+}
+
---
+++ RRQMXML/Form1.cs
@@ -0,0 +1,83 @@
+﻿using RRQMXml;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RRQMXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        XmlTool xml = new XmlTool("App.xml");
+
+        /// <summary>
+        /// 测试生成Xml按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testGenerateXmlBtn_Click(object sender, EventArgs e)
+        {
+            //创建方法一：节点+属性名+属性值
+            //xml.AttributeStorage("student","name","张三");
+
+            //创建方法二：节点+属性名集合+属性值集合
+            //string[] attribute_names = new string[] { "姓名", "性别", "年龄", "学号" };
+            //string[] attribute_values = new string[] { "张三", "男", "20", "10001" };
+            //xml.AttributeStorage("stdudent1", attribute_names, attribute_values);
+
+            //创建方法三：集合存储
+            //string[] nodenames = new string[] { "student2", "student3", "student4" };
+            //string[] attribute_values = new string[] { "张三2", "张三3", "张三4" };
+            //string[] attribute_names = new string[] { "姓名", "姓名", "姓名" };
+            //xml.AttributeStorage(nodenames, attribute_names, attribute_values);
+
+            //创建方法四：集合存储
+            string[] nodenames = new string[] { "student5", "student6", "student7", "student8", "student9", "student10" };
+            string[] attribute_names = new string[] { "姓名", "性别", "年龄", "学号" };
+            string[] name_values = new string[] { "张三5", "张三6", "张三7", "张三8", "张三9", "张三10" };
+            string[] gender_values = new string[] { "男", "男", "男", "女", "男", "女" };
+            string[] age_values = new string[] { "14", "16", "17", "19", "20", "21" };
+            string[] sno_values = new string[] { "1002", "1003", "1004", "1005", "1006", "1007" };
+            xml.AttributeStorage(nodenames, attribute_names,4, name_values, gender_values, age_values, sno_values);
+            
+        }
+
+        /// <summary>
+        /// 读取XML
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testReadXmlBtn_Click(object sender, EventArgs e)
+        {
+            //读取XML方法一：节点名+属性名取值
+            //string value = xml.SearchWords("student5", "姓名");
+            //txtXML.AppendText(value + "\r\n");
+
+            //读取XML方法二：已知属性名，属性值，然后获取其他属性值
+            string[] values = xml.SearchWords("姓名", "张三", "学号");
+            foreach (string  item in values)
+            {
+                txtXML.AppendText(item);
+            }
+
+            
+        }
+
+        void ExistXML()
+        {
+            xml.NodeExist("Node");//判断节点是否存在
+            xml.RemoveAllNode();//移除所有节点
+            xml.RemoveNode("nodeName");
+            xml.RemoveNode("AttributeName", DateTime.Now);//按时间删除当前时间以前的节点
+            xml.RemoveNode("AttributeName", "AttributeValue");//按属性名称，属性值 
+            Dictionary<string, string> keyValuePairs = xml.SearchAllAttributes("nodeName");//查找节点所有的属性，返回一个键值对
+            
+
+
+        }
+    }
+}
